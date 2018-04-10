8536cc1706ead15030b46cfa428cae4ef0218217
malei6 <malei6@guazi.com>
2018-04-10 12:01:48 +0800

    6.3

--- app/src/main/java/com/malei/mycenter/MainActivity.java
+++ app/src/main/java/com/malei/mycenter/MainActivity.java
@@ -10,7 +10,6 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-
-        //ml 6.2
+        //ml 6.3
     }
 }
